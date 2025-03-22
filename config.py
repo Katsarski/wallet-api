6ee83b0df22aba08901b492c60ac2ad27aa9c429
Nadezhda Kacarska <boyankatsarski@gmail.com>
2025-03-22 17:19:15 +0200

    extract X_SERVICE_ID from env variables

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
+import os
+
 """
 This module provides configuration variables, for the time being they are not environment specific 
 but can be further extended if needbe
 """
 
 BASE_URL = "https://challenge.test.local/challenge/api/v1"
-X_SERVICE_ID = "<your_service_id>"
+X_SERVICE_ID = os.getenv("X_SERVICE_ID", "<default_service_id>")
